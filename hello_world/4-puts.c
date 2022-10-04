8fa6077e210f87e4e59c36abc7b0201004d28e2d
cesarsanchez <cesarr2800@gmail.com>
2022-10-04 13:25:51 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,7 @@
-	/*programa0*/
-
-	#include<stdio.h>
-
-
-	int main()[
-
-		puts("Programming is like building a multilingual puzzle");
-
-		return 0;
-	]
+#include <stdio.h>
+	
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
