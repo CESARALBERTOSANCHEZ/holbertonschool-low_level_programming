4e0747fd4cfa6ced08cb1ad0effab9a41c16f8c0
cesarsanchez <cesarr2800@gmail.com>
2022-10-04 20:43:09 -0700

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+/**
+ * main - this function
+ * Return: always 0
+ */
+int main(void)
+[
+	puts("\"Programming is like building a multilingual puzzle");
 
-int main(void)[
-
-	puts write ("Programming is like building a multilingual puzzle");
-
-	return 0;
-	]	
+	return (0);
+]
