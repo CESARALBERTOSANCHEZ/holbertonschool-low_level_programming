0b2232b1dd7384a997d2ebdb4e6954896f8e4b6a
cesarsanchez <cesarr2800@gmail.com>
2022-10-17 23:00:36 -0700

    Write a function that checks for uppercase character.

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,1 +1,13 @@
-int _isupper(int c);
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c;
+	for (c = 'A'; c <= 'Z'; ++c)
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
