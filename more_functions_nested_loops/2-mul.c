b1f020f82723b6d8880e70069059adb02a55817d
CESARALBERTOSANCHEZ <cesarr2800@gmail.com>
2022-11-01 14:13:24 -0500

    2. Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * mul - multiply the caracter a y b
+ *  * @a: value
+ *  * Return: in a * b
+ *  * @b: value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
