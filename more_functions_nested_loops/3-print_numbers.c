0be07777e8866d5c96c29b9b58f1063871bb9b25
CESARALBERTOSANCHEZ <cesarr2800@gmail.com>
2022-11-05 18:17:02 -0500

    3. The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ ** print_numbers - printf numbers 0 the 9
+ ** Return: in 0
+ **/
+void print_numbers(void)
+{
+int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		printf ("%c", i);
+	}
+		putchar ('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _isupper(int c);
 int putchar(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
