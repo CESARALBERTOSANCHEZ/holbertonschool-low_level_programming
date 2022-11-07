f09522f2b18237d5634e5673c969e71b1c99f480
CESARALBERTOSANCHEZ <cesarr2800@gmail.com>
2022-11-06 23:20:17 -0500

    4. I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- ** print_numbers - printf numbers 0 the 9
+ ** print_most_numbers - printf numbers 0 the 9
  ** Return: in 0
  **/
 void print_most_numbers(void)
@@ -9,10 +9,10 @@ void print_most_numbers(void)
 int i;
 	for (i = 48; i < 58; i++)
 	{
-	if(i != 50 && i != 52)
+	if (i != 50 && i != 52)
 	{
-		printf("%c", i);
+		prinf("%c", i);
 	}
 	}
-		putchar('\n');    
+		putchar('\n');
 }
